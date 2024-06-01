8a2edf005cc0a5b319c4f9544207df9a3fe9ef88
Nikita Stepanov <klassgordon@gmail.com>
2024-06-01 16:25:00 +0300

    fixing the execution files for win and lin

--- Installation.bat
+++ Installation.bat
@@ -1,4 +1,6 @@
 @echo off
+python -m venv venv
+call venv\Scripts\activate.bat
 pip install -r requirements.txt
 cd tiktok_uploader/tiktok-signature/
 npm i
--- Installation.sh
+++ Installation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-python -m venv venv
+sudo apt install python3
+sudo apt install npm
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 cd tiktok_uploader/tiktok-signature/
